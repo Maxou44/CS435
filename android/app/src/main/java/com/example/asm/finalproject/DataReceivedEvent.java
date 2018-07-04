76dc799fe9e928366b20c64f4a1d505ff19424ce
Julien Karst 📱 <contact@julienkarst.com>
2018-07-04 10:34:21 -0700

    Revert "[WIP]"
    This reverts commit fb2b81b0af18788467344a10a790268021bbf81f.

---
+++ android/app/src/main/java/com/example/asm/finalproject/DataReceivedEvent.java
@@ -0,0 +1,21 @@
+package com.example.asm.finalproject;
+
+/**
+ * Created by mart_- on 03/07/2018.
+ */
+
+public class DataReceivedEvent {
+    private String data;
+
+    public DataReceivedEvent(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
---
+++ android/app/src/main/java/com/example/asm/finalproject/OnClickDeviceItemListener.java
@@ -0,0 +1,9 @@
+package com.example.asm.finalproject;
+
+/**
+ * Created by mart_- on 20/06/2018.
+ */
+
+public interface OnClickDeviceItemListener {
+    void OnClickDeviceItem(DeviceItem deviceItem);
+}
