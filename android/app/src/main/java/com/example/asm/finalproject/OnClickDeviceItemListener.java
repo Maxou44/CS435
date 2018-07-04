fb2b81b0af18788467344a10a790268021bbf81f
Julien Karst 📱 <contact@julienkarst.com>
2018-07-04 10:34:02 -0700

    [WIP]

--- android/app/src/main/java/com/example/asm/finalproject/OnClickDeviceItemListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.asm.finalproject;
-
-/**
- * Created by mart_- on 20/06/2018.
- */
-
-public interface OnClickDeviceItemListener {
-    void OnClickDeviceItem(DeviceItem deviceItem);
-}
